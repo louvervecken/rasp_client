166de93b819a2ff946d7e8f144a6aa20f242e911
root <root@raspberrypi.(none)>
2014-01-12 22:15:00 +0000

    Sending cpu temp, ram and storage data. Starting and stopping motion functional

--- main.py
+++ main.py
@@ -3,37 +3,44 @@ import datetime
 import time
 
 from subprocess import Popen
-from rasp_client.system_status import *
+from system_status import *
 
 alarm_enabled = False
 
 
 
 def enable_motion():
-    return subprocess.Popen(['motion'])
+    return Popen(['motion'])
 
     
-while True
-    # get alarm enabled config from server
-    alarm_config = requests.get('https://rasp-lou-server.appspot.com/alarm-config/get')
-    if alarm_config.status_code == 200:
-        if alarm_config.text == u'alarm_enabled = True':
-            if alarm_enabled is False:
-                motion_process = enable_motion()
-                alarm_enabled = True
-        elif alarm_config.text == u'alarm_enabled = False':
-            if alarm_enabled is True:
-                motion_process.terminate()
-                alarm_enabled = False
+def run():
+    while True:
+        global alarm_enabled
+        # get alarm enabled config from server
+        alarm_config = requests.get('https://rasp-lou-server.appspot.com/alarm-config/get')
+        if alarm_config.status_code == 200:
+            if alarm_config.text == u'alarm_enabled = True':
+                if alarm_enabled is False:
+                    motion_process = enable_motion()
+                    alarm_enabled = True
+            elif alarm_config.text == u'alarm_enabled = False':
+                if alarm_enabled is True:
+                   motion_process.terminate()
+                   alarm_enabled = False
+            else:
+                raise ValueError("Unrecognized reply when getting alarm_config: {}".format(alarm_config.text))
         else:
-            raise ValueError("Unrecognized reply when getting alarm_config: {}".format(alarm_config.text))
-    else:
-        raise IOError("Error getting alarm config, error code: {}".format(alarm_config.status_code))
+            raise IOError("Error getting alarm config, error code: {}".format(alarm_config.status_code))
     
-    cpu_temp = get_cpu_temperature()
-    r = requests.post('https://rasp-lou-server.appspot.com/data-posting',
-                      data={'cpu_temp': 44.3})
-    time.sleep(10)
+        cpu_temp = get_cpu_temperature()
+        ram_perc = get_ram_usage()[3]
+        free_storage = round(get_storage_usage()[2], 3)
+        r = requests.post('https://rasp-lou-server.appspot.com/data-posting',
+                          data={'cpu_temp': cpu_temp,
+                                'ram_perc': ram_perc,
+                                'free_storage': free_storage})
+        print('{}-{}'.format(cpu_temp, r))
+        time.sleep(10)
     
     
     
--- system_status.py
+++ system_status.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import psutil
 from subprocess import Popen, PIPE
 
